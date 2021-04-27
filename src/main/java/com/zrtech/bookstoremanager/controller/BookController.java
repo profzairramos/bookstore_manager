d9cf07bd629b7e8a8f50b78c91c7de7ec9ed31a9
Prof Zair Ramos <prof.zair.ramos@gmail.com>
2021-04-27 20:28:55 -0300

    atualizando o git

--- src/main/java/com/zrtech/bookstoremanager/controller/BookController.java
+++ src/main/java/com/zrtech/bookstoremanager/controller/BookController.java
@@ -1,5 +1,8 @@
 package com.zrtech.bookstoremanager.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/books")
 public class BookController {
 
+    @ApiOperation(value = "Método de Demonstração de Recurso de API - Hello World")
+    @ApiResponses(value = {
+            @ApiResponse( code = 200, message = "Método retornou com sucesso!")
+    })
     @GetMapping
     public String hello(){
-        return "Hello World  -  BookStore Manager";    }
+        return "Hello World  -  BookStore Manager";
+    }
 }
